4e53bfb43bcc5ee7f36d8b4ebaf9c80e76996536
Henrique de Melo Jorge Barbosa <hmj.barbosa+git@gmail.com>
2022-10-31 12:15:59 -0400

    tentative new version with a bunch of bug fixes. output to prepro4.

--- calib_function_v1.m
+++ calib_function_v1.m
@@ -1,100 +1,100 @@
 % calib help
-function [constante_cfit,gof,output] = ...
+function [fitAB, fitA] = ... 
         calib_function_v1(sonde_alt, sonde_w,...
                           altitude, Pn2, Ph2o,...
-                          jdi, jdf, bg_n2_std, bg_h2o_std)
+                          jdi, jdf, snr_n2, snr_h2o)
+    % bug: using bg_std is incorrect
+    %                          jdi, jdf, bg_n2_std, bg_h2o_std)
 
 
+
+%% calculate h2o as ratio of Ph2o and Pn2
+
+% smooth parameter
 sm = 11;
 
 sm_h2o = smooth(Ph2o,sm);
 sm_n2 = smooth(Pn2,sm);
-% sm_n2_stdm = Pn2_stdm./sm.^0.5;
-%sm_n2_stdm = Pn2_stdm;
-% sm_n2_std = bg_n2_std
-% num_std = bg_h2o_std
-%sm_n2_stdm = bg_stdm_n2;
-h2o = sm_h2o./sm_n2  ; % PC_H2o / N2
+sm_h2o = sm_h2o./sm_n2  ; % PC_H2o / Glue_N2
 
-%mask_sm_n2 = (sm_n2./sm_n2_std) < 10;
-mask_sm = ((sm_h2o./bg_h2o_std) < 10) | ((sm_n2./bg_n2_std) < 10) ;
-mask_sm = mask_sm | altitude>10000 | altitude<200;
-h2o(mask_sm) = NaN;
+% without smooth, just for plotting
+h2o=Ph2o./Pn2;
 
-figure (333);
-clf
-hold on
-plot(altitude,sm_n2)
-plot(altitude(mask_sm),sm_n2(mask_sm),'o')
-xlim([0,15000])
-hold off
+%% Region for calibration
 
-figure(334)
-clf
-hold on 
-plot(altitude,sm_h2o)
-plot(altitude(mask_sm),sm_h2o(mask_sm),'o')
-xlim([0,15000])
-hold off
+% initial range
+calib_H_inf = 1600;
+calib_H_sup = 10000;
 
+%mask_sm = ((sm_h2o./bg_h2o_std) < 10) | ((sm_n2./bg_n2_std) < 10) ;
+mask_sm = (snr_n2 < 3); % | (snr_h2o < 10);
+mask_sm = mask_sm | altitude>calib_H_sup | altitude<calib_H_inf;
+
+% Plot SNR and region used for calibration
+figure (123);
+clf; hold on; grid on; box on
+plot(snr_h2o,altitude/1000,'b'); 
+plot(snr_n2 ,altitude/1000,'r'); 
+plot(snr_h2o(mask_sm),altitude(mask_sm)/1000,'xb')
+plot(snr_n2 (mask_sm),altitude(mask_sm)/1000,'xr')
+legend('h2o pc','n2 glue')
+xlabel('Signal / Noise')
+set(gca,'xscale','log')
+yl=ylabel('Altitude a.s.l. (km)');
+set(yl,'units','normalized','position',[-0.05 0.5 0])
+
+% Plot the uncalibrated lidar signal
 figure (730)
 clf; hold on; grid on; box on
 plot(h2o,altitude/1000)
+plot(sm_h2o,altitude/1000)
 xlabel('Uncalibrated lidar mixing ratio (a.u.) ')
 yl=ylabel('Altitude a.s.l. (km)');
 set(yl,'units','normalized','position',[-0.05 0.5 0])
+xlim([0 0.020])
 ylim([0 7])
 t=text(.02, .02, '  b  ', 'units', 'normalize',...
        'Horiz','left','Vert', 'bot',...
        'backgroundcolor','white','edgecolor','black','fontsize',12);
 
-% [sm_n2(1000:1500),altitude(1000:1500)./1000,sm_h2o(1000:1500),h2o(1000:1500)]
-% bg_n2_std
-% h2o = smooth(Ph2o,sm) ./ smooth(Pn2,sm)  ; % PC_H2o / N2
-% % h2o = Ph2o ./ Pn2  ; % PC_H2o / N2
 
+%% Apply mask to crop out the calibration region
 
-% mask_aux = ~isnan(h2o);
-% h2o_lidar = interp1q(altitude(mask_aux),h2o(mask_aux), sonde_alt);
+%sm_h2o(mask_sm) = NaN;
+%h2o(mask_sm) = NaN; 
 
-h2o_lidar = interp1q(altitude,h2o, sonde_alt);
-% [h2o_lidar.*1000,sonde_alt./1000,sonde_w]
-% h2o_lidar = interp1q(smooth(altitude,sm),smooth(h2o,sm), sonde_alt);
+% update range to crop sonde data
+altlim = altitude(~mask_sm);
+calib_H_inf = altlim(1);
+calib_H_sup = altlim(end);
 
-calib_H_inf = 200;        
-% mask_sinal = h2o_lidar<0 
-% sup = sonde_alt(mask_sinal)
-sup = altitude(mask_sm & altitude>500);
-sup(1)
-sup(end)
-if numel(sup)>0
-    calib_H_sup = sup(1) - 50 ;
-else
-    calib_H_sup = 10000;
-end
-%calib_H_sup = 10000
-% calib_H_sup = 3500;
-disp(sprintf('calib_H_sup = %f',calib_H_sup))
-mask = sonde_alt > calib_H_inf & sonde_alt < calib_H_sup ;
+disp(sprintf('calib_H = [%f, %f]',calib_H_inf, calib_H_sup))
+
+% interpolate to sonde altitudes
+h2o_lidar = interp1q(altitude,sm_h2o, sonde_alt);
+
+% mask sonde data (to remove the NaN above)
+mask = (sonde_alt > calib_H_inf) & (sonde_alt < calib_H_sup) ;
 
 X = h2o_lidar(mask);
 Y = sonde_w(mask);
 
-% size(sonde_alt)
-% size(mask)
-% [sonde_alt/1000 mask h2o_lidar*1000 sonde_w ]
+% fit with A*X
+myfunc = fittype( @(p1,x) p1*x );
+[constante_cfit,gof,output] = fit(X,Y,myfunc, 'startpoint', [750.]);
+fitA.cfit = constante_cfit;
+fitA.gof  = gof;
+fitA.out  = output; 
+fitA.cfit
 
-% mask = ~isnan(X) & ~isnan(Y);
-%[X Y mask]
-%constante_cfit = fit(X(mask),Y(mask),'poly1')
-%myfunc = fittype( @(p1,x) p1*x )
-%[constante_cfit,gof,output] = fit(X,Y,myfunc)
-[constante_cfit,gof,output] = fit(X,Y,'poly1')
+% fit with A*X + B
+[constante_cfit,gof,output] = fit(X,Y,'poly1');
+fitAB.cfit = constante_cfit;
+fitAB.gof  = gof;
+fitAB.out  = output; 
+fitAB.cfit
 
-% constante_calib.p2 = cfun_calib.p2;
-% constante_calib.p1 = cfun_calib;
-
-
+clear constante_cfit gof output
 
 %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%
@@ -129,10 +129,18 @@ grid on
 figure(1540)
 clf; hold on; grid on; box on
 plot(X,Y,'*b')
-% plot(constante_cfit)
-%plot(a)
-plot(constante_cfit,'predfunc')
-legend('Data','Fit','Location','northeast');
+
+plot(fitAB.cfit,'predfunc')
+fitresultAB = evalc('fitAB.cfit');
+text(0.45,0.2, fitresultAB(21:end-1),'units','normalized', ...
+     'Interpreter','none','backgroundcolor','white','edgecolor','black')
+
+plot(fitA.cfit,'g') % 'predfunc')
+fitresultA = evalc('fitA.cfit');
+text(0.11,0.8, fitresultA(23:end-1),'units','normalized', ...
+     'Interpreter','none','backgroundcolor','white','edgecolor','black')
+
+legend('Data','Fit A*X+B','Fit A*X','Location','northeast');
 %legend1 = legend(gca,'show');
 %set(legend1,'Location','southeast');
 xlabel('Uncalibrated lidar mixing ratio (a.u.)')
@@ -140,7 +148,8 @@ yl=ylabel('Sounding mixing ratio (g/kg)');
 set(yl,'units','normalized','position',[-0.07 0.5 0])
 xticks(0:0.004:0.02)
 yticks(0:4:20)
-ylim([0 20])
+xlim([0 0.016])
+ylim([0 16])
 t=text(.98, .02, '  a  ', 'units', 'normalize',...
        'Horiz','right','Vert', 'bot',...
        'backgroundcolor','white','edgecolor','black','fontsize',12);
@@ -149,25 +158,28 @@ t=text(.98, .02, '  a  ', 'units', 'normalize',...
 figure(1541)
 clf
 hold on
-plot(X,Y-constante_cfit(X),'ob')
+plot(X,Y-fitAB.cfit(X),'ob')
 hold off
 % legend('Razão de Mistura de H2O sonda')
 title(['data from ' datestr(jdi) ' to ' datestr(jdf)])
 xlabel('Razão de Mistura Lidar Não Calibrado')
 ylabel('Residuo [g/kg]')
 grid on
- %%
- 
-disp(['p1 = ' num2str(constante_cfit.p1) ])
-%disp(['p2 = ' num2str(constante_cfit.p2) ])
+
+
 figure (1542)
 clf; hold on; grid on; box on
-%plot(h2o.*constante_cfit.p1, altitude/1000,'b')
-plot(h2o.*constante_cfit.p1 + constante_cfit.p2, altitude/1000,'b')
+plot(h2o.*fitAB.cfit.p1 + fitAB.cfit.p2, altitude/1000,'b')
+%plot(sm_h2o.*fitAB.cfit.p1 + fitAB.cfit.p2, altitude/1000,'b')
 plot(sonde_w,sonde_alt./1000,'o-r')
-plot(h2o.*829, altitude/1000,'-k')
-plot(h2o.*(829+82), altitude/1000,':','color','#909090')
-plot(h2o.*(829-82), altitude/1000,':','color','#909090')
+%plot(h2o.*829, altitude/1000,'-k')
+%plot(h2o.*(829+82), altitude/1000,':','color','#909090')
+%plot(h2o.*(829-82), altitude/1000,':','color','#909090')
+
+plot(h2o_lidar.*fitAB.cfit.p1 + fitAB.cfit.p2, sonde_alt./1000,'sm')
+plot(h2o_lidar(mask).*fitAB.cfit.p1 + fitAB.cfit.p2, sonde_alt(mask)./1000,'sm','MarkerFaceColor','m')
+plot(sonde_w(mask),sonde_alt(mask)./1000,'or','MarkerFaceColor','r')
+
 legend('Calibrated lidar profile','Reference sounding','Best estimate')
 xlabel('Mixing ratio (g/kg)')
 yl=ylabel('Altitude a.s.l. (km)');
--- calibracao_v2.m
+++ calibracao_v2.m
@@ -1,262 +1,311 @@
 %%
-%close all
-%fclose all
+% close all
+% fclose all
 clear all
 
 % change display for all plots
 set(0, 'DefaultAxesFontSize', 12, 'DefaultLineLineWidth', 2)
 %%
-%addpath('../IC Henrique/')
-%addpath('../data/')
-addpath('../matlab/sc/')
-%%
+
+addpath('./libs/')
+addpath('./sc/')
+
+%% Read sonde data 
 load('sonde.mat')
 
 jd_sonde_utc = [sonde(:).jd];
-jd_sonde = jd_sonde_utc - datenum(00,00,00,04,00,00);   % hora local
+jd_sonde = jd_sonde_utc - datenum(00,00,00,04,00,00);   % local time
 
+%% Paths for data processing
 
-%%
+% datadir
+datadir='/Users/hbarbosa/DATA/lidar/data/';
 
-% abrir varios arquivos
-%     datadir = 'data';
-%datadir = 'F:\Dados lidars\lidar\data' 
-% datadir = 'D:\Dados\lidar\data';
-datadir = '/Users/hbarbosa/DATA/lidar/data'
-%%
+figure_dir = './hmjb/figures/';
+prepro_dir = './prepro4/';
 
-figure_dir = './prepro2/figures/';
-prepro_dir = './prepro2/';
+%% read DC 
 
-%% ler planilha em format CSV
-%% deve ter pelo menos colunas para altura maxima e minima
+% old: fixed dbin=10 for all channels
+% dbin_MAO = [10];
+% new: 
+dbin_MAO = [9 -0 9 -1 -1];
+dtime_MAO =  0.004;
 
-%% Y M D H Hmin Hmax FitBom? NumvenBaixa(1/0) Media Alta 
-%%
-%%
-
-%%
+DCdir='2012_11_29_telescope/Dark_Current_19h34_night/';
+[nfileDC, headDC, phyDC] = profile_read_dir([datadir DCdir], dbin_MAO, dtime_MAO);
+    
+%% Loop over all dates to be processed
 
 for yu = datenum(2011,08,27,20,00,00):datenum(2011,08,27,20,00,00)
     
-    clearvars -except yu sonde jd_sonde jd_sonde_utc datadir figure_dir prepro_dir
+    clearvars -except yu sonde jd_sonde jd_sonde_utc datadir figure_dir prepro_dir ...
+        dbin_MAO dtime_MAO nfileDC headDC phyDC
     
-    %%
+    % --------------------------------------------------------------------------
+    % RADIO SONDE
+    % --------------------------------------------------------------------------
+
+    % find closest sounding (in time)
+    [sonde_dist_dias, I_sonde]= min(abs(jd_sonde - yu));
     
-    
-    [sonde_dist_dias, I_sonde]= min(abs(jd_sonde - yu))
-    
-    
+    % skip if more than 1 day
     if sonde_dist_dias > 0
-        
         continue
-        
     end
-    %%
     
-    
-    % jdi=datenum(2014,09,14,20,00,00);       % data inicial
-    % jdf= jdi + 1/24;                        % data final
-    
-   
+    % select lidar data around time of sounding
     jdi = jd_sonde(I_sonde) - 0.5/24;
-    jdf = jdi + 1/24;
+    jdf = jdi + 1./24;
     
+    altitude_sonde = sonde(I_sonde).alt;  % [m]
+    wvmix_sonde = sonde(I_sonde).MIXR;    % [g/kg];
     
-    % dbin_MAO = [10];
-    dbin_MAO = [9 -0 9 -1 -1];
-    dtime_MAO =  0.004;
-    [nfile, head, phy] = profile_read_dates(datadir, jdi, jdf, dbin_MAO, dtime_MAO)
+    mask = altitude_sonde >= 0 & wvmix_sonde >= 0;
     
+    altitude_sonde = altitude_sonde(mask);
+    wvmix_sonde = wvmix_sonde(mask);    
+   
+    figure(50)
+    clf; hold on; grid on; box on;
+    plot(sonde(I_sonde).RH, sonde(I_sonde).alt/1e3)
+    xlabel('Relative Humidity [%]')
+    ylabel('Altitude asl [km]')
+
     
+    % --------------------------------------------------------------------------
+    % LIDAR
+    % --------------------------------------------------------------------------
+        
+    % read lidar data around time of sounding
+    [nfile, head, phy] = profile_read_dates(datadir, jdi, jdf, dbin_MAO, dtime_MAO);
+    
+    % skip if there are no lidar files
     if nfile == 0
         continue
     end
-   
-    %%
-    % alturas
+        
+    % range (m)
     range(:,1)=head(1).ch(1).binw*(1:head(1).ch(1).ndata);
     
-    %%
-    % altitude
+    % altitude a.s.l. (m) 
+    % note: lidar is tiled 5deg to avoid specular reflection on ice crystals
     altitude = range.*cosd(5) + head(1).alt;
     
-    %%
-    % horarios dos arquivos
+    % time (julian date)
     for i=1:numel(head)
         jd(i)=head(i).jdi;
     end
     
-    %% Subtrair BG do sinal bruto
+    %% BG removal 
     
     % mask_bg = size(phy(ch).data,1)-1000:size(phy(ch).data,1);
     mask_bg = range > range(end-1000) & range < range(end);
-    
+        
     for ch=1:head(1).nch
         phy(ch).bgcorr = phy(ch).data.*NaN;
+        % crop DC to current length
+        DC(:,ch) = mean(phyDC(ch).data(1:head(1).ch(1).ndata, :), 2);
+        % make sure DC has zero average
+        DC(:,ch) = DC(:,ch) - mean(DC(mask_bg,ch));
         for jj = 1:length(jd)
-            phy(ch).bgcorr(:,jj) = phy(ch).data(:,jj) - mean(phy(ch).data(mask_bg,jj));
+            phy(ch).bg(jj) = mean(phy(ch).data(mask_bg,jj));
+            if (head(1).ch(ch).photons==0)
+                phy(ch).bgcorr(:,jj) = phy(ch).data(:,jj) - phy(ch).bg(jj) - DC(:,ch);
+            else
+                phy(ch).bgcorr(:,jj) = phy(ch).data(:,jj);
+            end
         end
     end
     
+    R2mat = (range*ones(1,length(jd))).^2;
     
-    %%
-    aux = phy(1).bgcorr.*(range*ones(1,length(jd))).^2;
-    
-    r_aux = range<20000;
-    figure(11)
-    clf
-    %set(gcf,'units','normalized','outerposition',[0 0 0.8 1])
-    
-    [MAT_aux,jd_aux] = crop_tempo_0(aux,jd,1,1); %%% Essa função esta errada (bem porca). Arrumar depois
-    gplot2(MAT_aux(r_aux,:),[0:1:10].*1e6, jd_aux,range(r_aux)/1.e3)
-    % gplot2(aux,[0:0.1:1.5].*1e7, jd,alt/1.e3)
-    %title(['data from ' datestr(jd(1)) ' to ' datestr(jd(end))])
-    title('Sinal do lidar ao longo de 1 hora')
-    ylabel('Altura [km]')
-    xlabel('Horário [LT]')
-    ylim([0 15])
-    datetick('x','keeplimits')
-    
-    hc = colorbar;
-    ylabel(hc,'Sinal corrigido pela distância [U.A.]','fontsize',14)
-    
-    saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_RCS_2D_.png' ])
-    
-    % pause
-    %%
-    
-    %%
-    %tirando o bg
-    for i=1:head(1).nch
-        PcomBG(:,i) = mean(phy(i).data(:,:),2);
-        PcomBG_std(:,i) = std(phy(i).data(:,:),1,2);
-        PcomBG_stdm(:,i) = std(phy(i).data(:,:),1,2)./size(phy(i).data(:,:),2).^0.5;
-        
-        bg(i) = mean(PcomBG(mask_bg,i));
-        desvio(i) = std(PcomBG(mask_bg,i), 1);
-        bg_stdm(i) = std(PcomBG(mask_bg,i), 1)./size(PcomBG(mask_bg,i),1).^0.5;
-        P(:,i) = PcomBG(:,i) - bg(i);
-        Pr2(:,i) = P(:,i).*range.*range;
+    % PLOT Signal x Time
+    for ch=1:head(1).nch
+        figure(10+ch); clf; 
+        hold on; grid on; box on
+        tmp = log10(phy(ch).bgcorr.*R2mat);
+        tmp(phy(ch).bgcorr<=0) = nan;
+        [h, bar]=gplot2(tmp,[], jd, range/1.e3);
+        %set(gca,'colorscale','log') 
+        if (head(1).ch(ch).photons==1) 
+            tag=[num2str(head(1).ch(ch).wlen) '_PC'];
+        else
+            tag=[num2str(head(1).ch(ch).wlen) '_AN']; 
+        end
+        title([datestr(jd(1),'yyyy/mm/dd') ' ' tag],'interpreter','none')
+        ylabel('Altitude [km]')
+        xlabel('Time [LT]')
+        ylim([0 15])
+        datetick('x','keeplimits')    
+        ylabel(bar,'Log10 RCS [A.U.]','fontsize',14)
+        %saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_RCS_2D_' tag '.png' ])
     end
     
+    for ch=1:head(1).nch
+        % mean P, std P, std of the mean P
+        P_withBG(:,ch) = mean(phy(ch).data(:,:),2);
+        P_withBG_std(:,ch) = std(phy(ch).data(:,:),1,2);
+        
+        % mean BG, std BG, std of the mean BG
+        bg(ch) = mean(P_withBG(mask_bg,ch));
+        bg_std(ch) = std(P_withBG(mask_bg,ch), 1);
+
+        % correct for BG if channel is AN
+        % note: WV calib at night only, and PC has no BG
+        if (head(1).ch(ch).photons==0)
+            P(:,ch) = P_withBG(:,ch) - bg(ch) - DC(:,ch);
+            SNR(:,ch) = P(:,ch)./P_withBG_std(:,ch);
+        else
+            P(:,ch) = P_withBG(:,ch);
+            % get total number of photons
+            % poisson uncertainty is sqrt(N), hence SNR is N/sqrt(N) = sqrt(N)
+            SNR(:,ch) = sqrt(P(:,ch)* head(1).nshoots / 20);
+        end
+        
+        % RCS
+        Pr2(:,ch) = P(:,ch).*range.*range;
+    end
     
-    %% achar a regiao onde vale o photocount e o analogic
-    ANmask = PcomBG(:,3) > 2.06 & PcomBG(:,3) < 50 & range > 100; % em mV do AN
-    sum(ANmask)
-    PCmask = PcomBG(:,4) < 12.; % em MHz do PC
-    sum(PCmask)
+    % PLOT MEAN SIGNAL / Signal-to-noise ratio
+    for ch=1:head(1).nch
+        figure(20+ch); clf; 
+        subplot(1,2,1)
+        hold on; grid on; box on
+        plot(range.*range.*(P(:,ch)), range/1.e3, 'b-')
+        if (head(1).ch(ch).photons==1) 
+            tag=[num2str(head(1).ch(ch).wlen) '_PC'];
+        else
+            tag=[num2str(head(1).ch(ch).wlen) '_AN']; 
+        end
+        title([datestr(jd(1),'yyyy/mm/dd') ' ' tag],'interpreter','none')
+        ylabel('Altitude [km]')
+        xlabel('RCS')
+        set(gca,'xscale','log')
+        ylim([0 30])
+        
+        subplot(1,2,2)
+        plot(SNR(:,ch), range/1.e3, 'r-')
+        hold on; grid on; box on
+        xlabel('S/N ratio')
+        title(['SNR(5km)=' num2str(mean(SNR(650:680,ch)))])
+        %saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_RCS_1D_' tag '.png' ])
+    end
+        
+    %% Gluing
     
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    X_glue = P(ANmask & PCmask,3);
-    Y_glue = P(ANmask & PCmask,4);
+    % find region where PC and AN are valid
+    %ANmask = P_withBG(:,3) > 2.005 & P_withBG(:,3) < 50; 
+    %PCmask = P_withBG(:,4) < 12.; 
+    maxAN = head(1).ch(3).discr*1e3; % mV
+    %resolAN = maxAN/2^head(1).ch(3).bits; % mV
+    ANmask = (P_withBG(:,3) > bg(3) + bg_std(3)*10 ) & (P_withBG(:,3) < maxAN/2.); 
+    PCmask = P_withBG(:,4) < 45.; 
+    Gmask = ANmask & PCmask & range > 100.; 
+    
+    X_glue = P(Gmask,3);
+    Y_glue = P(Gmask,4);
     cfun_glue = fit(X_glue,Y_glue,'poly1')
-    
     
     Signal_glued = cfun_glue(P(:,3));
     Signal_glued(PCmask) = P(PCmask,4);
-    %sinal máximo perto do chão 
+
+    % snr from glue
+    SNR_glue = sqrt(Signal_glued* head(1).nshoots / 20);
+
+    % Get signal near ground x PBL
+    % to exclude fog near ground
     a = floor(250/7.5);
     b = floor(3000/7.5);
     s_max250 = max(Signal_glued(1:a));
     s_max3000 = max(Signal_glued(a:b));
     
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
     figure(100)
-    clf
-    hold on
+    clf; hold on; grid on; box on
     plot(X_glue,Y_glue,'.r')
     plot(cfun_glue,'b')
-    hold off
+    fitresult = evalc('cfun_glue');
+    text(0.45,0.2, fitresult(21:end-1),'units','normalized', ...
+         'Interpreter','none','backgroundcolor','white','edgecolor','black')
     title(['data from ' datestr(jd(1)) ' to ' datestr(jd(end))])
     xlabel('AN Signal [mV]')
     ylabel('PC Signal [MHz]')
-    grid on
+    saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_fit_glue.png' ])
     
-    saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_fit_glue_.png' ])
-    
-    
-    %%
+    figure(101)
+    clf; hold on; grid on; box on
+    plot(X_glue,Y_glue - cfun_glue(X_glue),'or')
+    xlabel('AN Signal [mV]')
+    ylabel('Residue [MHz]')
+    set(gca,'xscale','log')
+    saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_fit_glue_resid.png' ])
     
     figure(110)
-    clf
-    hold on
-    plot(range/1000,P(:,3),range/1000,P(:,4),range/1000,Signal_glued,'k')
-    plot(range(ANmask & PCmask)/1000,P(ANmask & PCmask,3),'o',range(ANmask & PCmask)/1000,P(ANmask & PCmask,4),'o')
-    plot(range/1000,P(:,5),'c')
-   %plot([0,15],[1,1].*bg(4),'-r')
-    plot([0,15],[1,1].*(3*desvio(4)),'--r')
-    plot([0,15],[1,1].*(10*desvio(4)),'--r')
-    %plot([0,15],[1,1].*bg(5),'-b')
-    plot([0,15],[1,1].*(3*desvio(5)),'--b')
-    plot([0,15],[1,1].*(10*desvio(5)),'--b')
-    hold off
-    legend('AN','PC','Glued','Fit region AN','Fit region PC')
-    grid on
-    set(gca,'yscale','log')
-    xlim([0 15])
-    title(['data from ' datestr(jd(1)) ' to ' datestr(jd(end))])
-    xlabel('Altura [m]')
-    ylabel(['Sinal PC ou AN [MHz ou mv]'])
-    
-    saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_Signal_glue_.png' ])
-    
-    %%
-    
-    figure(110000)
-    clf
-    hold on
-    plot(range/1000,P(:,3),range/1000,P(:,4),range/1000,Signal_glued,'k')
-    plot(range(ANmask & PCmask)/1000,P(ANmask & PCmask,3),'o',range(ANmask & PCmask)/1000,P(ANmask & PCmask,4),'o')
-    plot(range/1000,smooth(P(:,4),11) + PcomBG_stdm(:,4),'r')
-    hold off
-    legend('AN','PC','Glued','Fit region AN','Fit region PC')
-    grid on
-    set(gca,'yscale','log')
-    xlim([0 15])
-    title(['data from ' datestr(jd(1)) ' to ' datestr(jd(end))])
-    xlabel('Altura [m]')
-    ylabel(['Sinal PC ou AN [MHz ou mv]'])
-    
-    
-%     figure(1100000)
-%     clf
-%     plot(range/1000,smooth(P(:,4),11)./PcomBG_stdm(:,4),'r')
-%     grid on
-    %%
-    % lendo a sondagem
-    
-    altitude_snd = sonde(I_sonde).alt;  % [m]
-    w_snd = sonde(I_sonde).MIXR;        % [g/kg];
-    
-    mask = altitude_snd >= 0 & w_snd >= 0;
-    
-    altitude_snd = altitude_snd(mask);
-    w_snd = w_snd(mask)   ; 
-   
-    
-    %%
-    
-    [constante_cfit,constante_gof,constante_output] = calib_function_v0(altitude_snd,w_snd,altitude,Signal_glued,P(:,5),jd(1),jd(end),PcomBG_stdm(:,4),desvio(4),desvio(5));
-    % [constante_cfit] = calib_function_v0(sonda(:,2),sonda(:,6),altitude,P(:,4),P(:,5));
+    clf; hold on; grid on; box on
+    % nitrogen
+    plot(range/1e3, P(:,3), 'b')      % AN 387
+    plot(range/1e3, P(:,4), 'r')      % PC 387 
+    plot(range/1e3, Signal_glued,'k') % Glue 387
+    plot(range(Gmask)/1e3, P(Gmask,3),'o') % region used in the gluing
+    plot(range(Gmask)/1e3, P(Gmask,4),'o') % region used in the gluing
+    % vapor
+    plot(range/1e3, P(:,5),'c') % PC 408
+    % bg for channels 387 and 408
+    % nao faz sentido o BG dos canais PC. so devemos olhar do AN.
+    %plot([0,30],[1,1].*bg(4),'-r')
+    plot([0,30],[1,1].*(3*bg_std(3)),'--b')
+    plot([0,30],[1,1].*(10*bg_std(3)),'--b')
+    %plot([0,30],[1,1].*bg(5),'-b')
+    %plot([0,30],[1,1].*(3*bg_std(5)),'--c')
+    %plot([0,30],[1,1].*(10*bg_std(5)),'--c')
+    legend('AN387','PC387','Glued387','Fit region AN','Fit region PC', 'PC408')
 
-    %,constante_gof,constante_output
-    %%
+    set(gca,'yscale','log')
+    xlim([0 30])
+    title(['data from ' datestr(jd(1)) ' to ' datestr(jd(end))])
+    xlabel('Range [m]')
+    ylabel(['Signal PC or AN [MHz or mv]'])
+    saveas(gca,[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_Signal_glue.png' ])
     
-    saveas(figure(1538),[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_lidar_signal_.png' ])
+    % --------------------------------------------------------------------------
+    % CALIBRATION
+    % --------------------------------------------------------------------------
+    
+    [fitAB,fitA] = ...
+        calib_function_v1(altitude_sonde, wvmix_sonde, ...
+                          altitude, Signal_glued, P(:,5), ...
+                          jd(1), jd(end), SNR_glue, SNR(:,5));
+    % bug: we should not use the STD of the BG, but instead the SNR
+    % of the signal at each point. 
+    %                      jd(1), jd(end), bg_std(4), bg_std(5));
+
+    fname=[datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_lidar_signal_'];
+    saveas(figure(1538),[figure_dir fname '.png' ])
+    set(figure(1538),'paperunits','points','papersize',[560 420])
+    print(figure(1538),[figure_dir fname '.pdf' ],'-dpdf','-bestfit')
+
     saveas(figure(1539),[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_MXR_sonde_mask_.png' ])
-    saveas(figure(1540),[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_fit_calib_.png' ])
+
+    fname=[datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_fit_calib_'];
+    saveas(figure(1540),[figure_dir fname '.png' ])
+    set(figure(1540),'paperunits','points','papersize',[560 420])
+    print(figure(1540),[figure_dir fname '.pdf' ],'-dpdf','-bestfit')
+        
     saveas(figure(1541),[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_residuos_fit_calib.png' ])
-    saveas(figure(1542),[figure_dir datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_Signal_calibrado.png' ])
+   
+    fname=[datestr(yu,'yyyy_mm_dd_HH_MM_SS') '_Signal_calibrado' ];
+    saveas(figure(1542),[figure_dir fname '.png' ])
+    set(figure(1542),'paperunits','points','papersize',[560 420])
+    print(figure(1542),[figure_dir fname '.pdf' ],'-dpdf','-bestfit')
+
     %%
     
     fname_save = [prepro_dir 'Calib_' datestr(yu,'yyyy_mm_dd_HH_MM_SS') '.mat' ];
-    save(fname_save,'constante_cfit','constante_gof','constante_output','altitude_snd','w_snd',...
-        'range','altitude','Signal_glued','P',...
-        'I_sonde','jd','s_max250','s_max3000')
-    
-%     pause
+    save(fname_save,'fitAB','fitA',...
+         'jd','jdi','altitude','range','Signal_glued','P',...
+         'altitude_sonde','wvmix_sonde','I_sonde', 'cfun_glue',...
+         's_max250','s_max3000')
     
 end
+
+%fim
